829bbdbc9317dd4343ad47f05159ef1c1e3fe0e3
Joe Wickes <joe.c.wickes@gmail.com>
2020-11-03 12:10:01 -0500

    Add smoke and snapshot testing to Card and List

---
+++ src/composition/List.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import List from './List';
+
+describe('List component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<List header="Title" cards={[
+      {
+        id: 1,
+        title: 'a',
+        content: 'b',
+      },{
+        id: 2,
+        title: 'c',
+        content: 'd',
+      }]} />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders the Card as expected', () => {
+    const tree = renderer
+      .create(<List header="Title" cards={[
+        {
+          id: 1,
+          title: 'a',
+          content: 'b',
+        },{
+          id: 2,
+          title: 'c',
+          content: 'd',
+        }]} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
